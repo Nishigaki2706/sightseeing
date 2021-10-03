1fe0807a6b787bfe03177ee176427298006da790
Nishigaki2706 <cn34927nc@gmail.com>
2021-10-03 09:08:09 +0900

    会員登録画面作成　CSS

--- sightseeing.js
+++ sightseeing.js
@@ -1,9 +1,31 @@
 'use strick';
 //メニュー開閉
+$(function(){
 $('.sideber').hide();
 $('header nav').on('click', function(){
-    console.log('Hello');
     $('.sideber').slideToggle();
 })
+});
 
-//console.log('Hello');
+
+
+//メニューバー　サンプルに飛ぶ
+// $(function(){
+//     document.getElementById('sample-position').addEventListener('click',()=>{
+//         document.getElementById('sample-position').classList('.move')
+//     });
+// });
+$(function(){
+    const top = document.getElementById('top');
+    top.addEventListener('click',() => {
+        $('.sideber').hide();
+    })
+})
+
+//トップに戻る、サンプルを押したらメニューバーが閉じる
+$(function(){
+    $('top').on('click',function() {
+        $('.sideber').hide();
+    });
+});
+
--- sign-up.php
+++ sign-up.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ja">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,13 +8,14 @@
     <title>会員登録</title>
 </head>
 <body>
-    <div class="singup">
+    <div class="sign-up">
+        <h1>会員登録する</h1>
         <form action="sign-in.php" method="post">
-            <input type="text" class="register" name="name" placeholder="名前を入力してください" maxlength="50" required>
+            <input type="text" class="register" name="name" placeholder="名前を入力してください" maxlength="50" required autofocus>
             <input type="text" class="register" name="nickname" placeholder="ニックネームを入力してください" maxlength="50" required>
             <input type="mail" class="register" name="mail" placeholder="メールアドレスを入力してください" maxlength="228" required>
             <input type="password" class="register" name="password" placeholder="パスワードを入力してください" minlength="4" maxlength="60" required>
-            <button type="submit">登録する</button>
+            <button type="submit"><a href="sign-in.php">登録する</a></button>
         </form>
     </div>
 </body>
